f6bf47daa6952b7f882d5565c4735384916a2ff6
matthewlazewski <matthew.lazewski@gmail.com>
2020-12-18 17:29:19 -0600

    cleaned up spacing

--- src/actions/result.js
+++ src/actions/result.js
@@ -41,14 +41,16 @@ import {
   export const initiateGetResult = (searchTerm) => {
     return async (dispatch) => {
       try {
-        const API_URL = `https://api.spotify.com/v1/search?query=${encodeURIComponent(
-          searchTerm
-        )}&type=album,playlist,artist`;
+        const API_URL = `https://api.spotify.com/v1/search?query=${encodeURIComponent(searchTerm)}&type=album,playlist,artist`;
+        
         const result = await get(API_URL);
         console.log(result);
+        
         const { albums, artists, playlists } = result;
+        
         dispatch(setAlbums(albums));
         dispatch(setArtists(artists));
+        
         return dispatch(setPlayList(playlists));
       } catch (error) {
         console.log('error', error);
